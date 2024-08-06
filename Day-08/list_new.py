1abefca182aedad983f168ef020afe630f2bbee1
Akshay Malik <125215478+Akki055@users.noreply.github.com>
2024-08-06 16:09:42 +0000

    added_list

---
+++ Day-08/list_new.py
@@ -0,0 +1,12 @@
+S3_bucket_list = ["abhi", "akki", "ramu", "chinu"]
+print(S3_bucket_list[0])
+print(len(S3_bucket_list))
+
+print(S3_bucket_list[0:3])
+
+
+S3_bucket_list.append("new_s3_bucket")
+print(S3_bucket_list)
+print(len(S3_bucket_list))
+S3_bucket_list.remove ("akki")
+print(S3_bucket_list)
